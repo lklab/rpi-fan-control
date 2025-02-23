e75b69cb62e2f33e80c192d59fceb1ca7e63bfc7
lklab <alexeibatic@gmail.com>
2025-02-23 21:01:20 +0900

    팬을 직접 제어하게 되면 60초간 on/off 상태 유지

--- controller/controller.py
+++ controller/controller.py
@@ -1,5 +1,6 @@
 import asyncio
 from datetime import datetime
+import time
 
 from data.fan_status import FanStatus
 from controller.platform_interface import PlatformInterface
@@ -9,6 +10,7 @@ class Controller :
         self.fan_status = fan_status
         self.fan_status.add_update_listener(self._on_updated)
         self.interface: PlatformInterface = PlatformInterface()
+        self.disable_time: float = 0.0
 
     def start(self) :
         asyncio.create_task(self._control_logic())
@@ -18,22 +20,25 @@ class Controller :
             temperature: float = self.interface.get_temperature()
             self.fan_status.current_temperature = temperature
 
-            # 팬을 끄는 시간 확인
-            if self._is_within_control_off_time(self.fan_status.control_off_time, self.fan_status.control_on_time) :
-                self.fan_status.is_fan_on = False
-
-            # 온도 범위 확인
-            else :
-                if self.interface.is_fan_on and temperature < self.fan_status.off_temperature :
+            if time.monotonic() > self.disable_time :
+                # 팬을 끄는 시간 확인
+                if self._is_within_control_off_time(self.fan_status.control_off_time, self.fan_status.control_on_time) :
                     self.fan_status.is_fan_on = False
-                elif not self.interface.is_fan_on and temperature > self.fan_status.on_temperature :
-                    self.fan_status.is_fan_on = True
+
+                # 온도 범위 확인
+                else :
+                    if self.interface.is_fan_on and temperature < self.fan_status.off_temperature :
+                        self.fan_status.is_fan_on = False
+                    elif not self.interface.is_fan_on and temperature > self.fan_status.on_temperature :
+                        self.fan_status.is_fan_on = True
 
             self.fan_status.update()
             await asyncio.sleep(1.0)
 
     def _on_updated(self) :
-        self.interface.set_fan_on(self.fan_status.is_fan_on)
+        if self.interface.is_fan_on != self.fan_status.is_fan_on :
+            self.disable_time = time.monotonic() + 60.0 # 팬을 직접 제어하게 되면 60초간 on/off 상태 유지
+            self.interface.set_fan_on(self.fan_status.is_fan_on)
 
     def _is_within_control_off_time(self, control_off_time: str, control_on_time: str) -> bool:
         now = datetime.now().time()
